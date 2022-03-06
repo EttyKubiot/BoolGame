a7ec572971cfb76c9487da254e4b905c42ba7ee9
EttyBlack <6166887@gmail.com>
2022-03-06 21:22:46 +0200

    2

--- Assets/DrawLine.cs
+++ Assets/DrawLine.cs
@@ -37,10 +37,10 @@ public class DrawLine : MonoBehaviour
 
         if (other.gameObject.CompareTag("start"))
         {
-            
+
             Debug.Log("start");
             isTrigger = true;
-            inTrigger = true;
+            //inTrigger = true;
 
         }
     }
@@ -59,43 +59,46 @@ public class DrawLine : MonoBehaviour
 
     //}
 
-   
+
     private void Update()
     {
+      
+            transform.position = mousePos;
 
-        transform.position = mousePos;
-       
             if (Input.GetMouseButtonDown(0))
             {
-            startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             }
 
-           
-           if ( Input.GetMouseButton(0))
-           {
 
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-            Line.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
-            distance = (mousePos - startMousePos).magnitude;
-
-           }
-                   
-
-            
-
-                
-            if(Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButton(0))
             {
-              
+
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                Line.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
+                distance = (mousePos - startMousePos).magnitude;
+                inTrigger = true;
             }
 
-        Swipe();
+            Swipe();
+
+       
+
 
     }
 
-   
+    public void LateUpdate()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("GetMouseButtonUp");
+            inTrigger = false;
+        }
+    }
+
+
 
 
 
@@ -110,83 +113,87 @@ public class DrawLine : MonoBehaviour
 
     public void Swipe()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (inTrigger)
         {
-            //save began touch 2d point
-            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            if (Input.GetMouseButtonDown(0))
+            {
+                //save began touch 2d point
+                firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                //save ended touch 2d point
+                secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+                //create vector from the two points
+                currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                //normalize the 2d vector
+                currentSwipe.Normalize();
+
+                //swipe upwards
+                if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(startMousePos.x, mousePos.y, 0f));
+                    transform.position = new Vector3(startMousePos.x, mousePos.y, 0f);
+
+                    Debug.Log("up swipe");
+                }
+                //swipe down
+                else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+                {
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(startMousePos.x, mousePos.y, 0f));
+                    Debug.Log("down swipe");
+                }
+                //swipe left
+                else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(mousePos.x, startMousePos.y, 0f));
+                    Debug.Log("left swipe");
+                }
+                //swipe right
+                else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(mousePos.x, startMousePos.y, 0f));
+                    Debug.Log("right swipe");
+                }
+
+                else if (currentSwipe.y > 0 && currentSwipe.x < 0)
+                {
+                    Debug.Log("1");
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(mousePos.x - 2f, mousePos.y - 2f, 0f));
+                }
+                // Swipe up right
+                else if (currentSwipe.y > 0 && currentSwipe.x > 0)
+                {
+                    Debug.Log("2");
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(mousePos.x + 2f, mousePos.y + 2f, 0f));
+                }
+                // Swipe down left
+                else if (currentSwipe.y < 0 && currentSwipe.x < 0)
+                {
+                    Debug.Log("3");
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(mousePos.x - 2f, mousePos.y - 2f, 0f));
+
+                    // Swipe down right
+                }
+                else if (currentSwipe.y < 0 && currentSwipe.x > 0)
+                {
+                    Debug.Log("4");
+                    Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+                    Line.SetPosition(1, new Vector3(mousePos.x - 2f, mousePos.y - 2f, 0f));
+                }
+            }
         }
-        if (Input.GetMouseButtonUp(0))
-        {
-            //save ended touch 2d point
-            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-
-            //create vector from the two points
-            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
-
-            //normalize the 2d vector
-            currentSwipe.Normalize();
-
-            //swipe upwards
-            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-            {
-               
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(startMousePos.x, mousePos.y, 0f));
-                transform.position = new Vector3(startMousePos.x, mousePos.y, 0f);
-
-                Debug.Log("up swipe");
-            }
-            //swipe down
-            else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
-            {
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(startMousePos.x, mousePos.y, 0f));
-                Debug.Log("down swipe");
-            }
-            //swipe left
-            else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
-            {
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(mousePos.x, startMousePos.y, 0f));
-                Debug.Log("left swipe");
-            }
-            //swipe right
-            else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
-            {
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(mousePos.x, startMousePos.y, 0f));
-                Debug.Log("right swipe");
-            }
-
-            else if (currentSwipe.y > 0 && currentSwipe.x < 0)
-            {
-                Debug.Log("1");
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(mousePos.x-2f, mousePos.y - 2f, 0f));
-            }
-            // Swipe up right
-            else if (currentSwipe.y > 0 && currentSwipe.x > 0)
-            {
-                Debug.Log("2");
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(mousePos.x + 2f, mousePos.y + 2f, 0f));
-            }
-            // Swipe down left
-            else if (currentSwipe.y < 0 && currentSwipe.x < 0)
-            {
-                Debug.Log("3");
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(mousePos.x - 2f, mousePos.y - 2f, 0f));
-
-                // Swipe down right
-            }
-            else if (currentSwipe.y < 0 && currentSwipe.x > 0)
-            {
-                Debug.Log("4");
-                Line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
-                Line.SetPosition(1, new Vector3(mousePos.x - 2f, mousePos.y - 2f, 0f));
-            }
-        }
+       
     }
 
 }
